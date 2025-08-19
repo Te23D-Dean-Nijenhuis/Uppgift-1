919b2f56b7db10c108a4caff11431baaf4fb2f28
Dean Nijenhuis <Dean.Nijenhuis@elev.ga.ntig.se>
2025-08-19 12:37:48 +0200

    Klar

---
+++ src/App.java
@@ -0,0 +1,54 @@
+import java.util.Random;
+import java.util.Scanner;
+
+
+public class App {
+    public static void main(String[] args) throws Exception {
+        
+        /*                                      BARA LEK
+    int[] antal = {0,0,0,0,0,0};
+    
+    for (int i = 0; i < 1000; i++) {
+            
+            Random tärning = new Random();
+    
+            int tal=tärning.nextInt(6)+1;
+            antal[tal-1]++;
+            
+        }
+        for (int i = 0; i < 6; i++) {
+            System.out.println(i+1 + "=" + antal[i]);
+        }
+        */
+
+        //gissa tal
+
+        Random Tal = new Random();
+
+        int Talgissa = Tal.nextInt(67)+1;
+
+        Scanner tb = new Scanner(System.in);
+
+        System.out.println("gissa tal");
+
+        int gissa = -100;
+
+        while (Talgissa != gissa) {
+            gissa = tb.nextInt();
+            if(gissa < Talgissa)
+            {
+                System.out.println("större");
+            } else if(gissa > Talgissa)
+            {
+                System.out.println("mindre");
+            } else
+            {
+                System.out.println("grattis!");
+            }
+        }
+
+
+
+
+    }
+}
